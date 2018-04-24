6bfd3d3c285fdd3f91c82e2ebe82b882be1851e6
buharov-alexander <buharovsasha@yandex.ru>
2018-04-24 22:32:51 +0700

    app: integrate immutable.js

--- joint_choice_app/app/components/movieList.js
+++ joint_choice_app/app/components/movieList.js
@@ -8,7 +8,7 @@ export default MovieList = ({ movies, navigation }) => {
     return (
         <List>
             {
-                Array.from(movies.values()).map(movie => (
+                movies.toList().map(movie => (
                     <ListItem
                         avatar={{ uri: 'http://192.168.1.100:8090/jointchoice/movie/poster/' + movie.id }}
                         key={movie.id}
--- joint_choice_app/app/reducers/movieReducer.js
+++ joint_choice_app/app/reducers/movieReducer.js
@@ -1,17 +1,18 @@
 import {
     LOAD_MOVIES_SUCCESS
 } from '../constants/actionTypes';
+import Immutable from 'immutable';
 
-const initialState = {moviesById: new Map()};
+
+const initialState = {moviesById: Immutable.Map()};
 
 export default function movieReducer(state = initialState, action) {
     switch (action.type) {
         case LOAD_MOVIES_SUCCESS:
-            const map = new Map();
-            action.payload.forEach(movie => {
-                map.set(movie.id, movie);
-            });
-            return Object.assign({}, state, { moviesById: map });
+            const moviesById = action.payload.reduce((map, movie) => {
+                return map.set(movie.id, movie);
+            }, Immutable.Map());
+            return Object.assign({}, state, { moviesById: moviesById });
         default:
             return state;
     }
