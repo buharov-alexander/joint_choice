fce37b3409a30c9935564206f5e9b0f7b9caa705
buharov-alexander <buharovsasha@yandex.ru>
2018-05-02 14:40:34 +0700

    app: disable eslint for loggingMiddleware

--- joint_choice_app/app/store/middleware/loggingMiddleware.js
+++ joint_choice_app/app/store/middleware/loggingMiddleware.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-unused-vars,no-console */
+
 const loggingMiddleware = store => next => (action) => {
   console.info('%cINFO:', 'color: green', `Dispatching a ${action.type} action with payload:`, action.payload);
-  const result = next(action);
+  return next(action);
 };
 
 export default loggingMiddleware;
+
+/* eslint-disable no-unused-vars,no-console */
